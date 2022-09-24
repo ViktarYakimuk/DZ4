7eba03fcca4ba377c20e705d382ab3b3d52d5003
ViktarYakimuk <ViktarYakimuk>
2022-09-24 20:50:16 +0300

    Третья задача.

---
+++ task03/Program.cs
@@ -0,0 +1,24 @@
+﻿//  Задача 3: Напишите программу, которая выводит массив из 8 элементов, 
+// заполненный случайными числами. 
+// 1,2,5,7,19,1,2,8 -> 1,2,5,7,19,1,2,8 
+//  -> 8
+
+int[] Create(int length)
+{
+   int[] Array = new int[8];
+   for (int i = 0; i < Array.Length; i++)
+   {
+      Array[i] = new Random().Next(1, 100);
+   }
+   return Array;
+}
+void PrintArray(int[] Array)
+{
+   for (int i = 0; i < Array.Length; i++)
+   {
+
+      System.Console.Write($"{Array[i]}\t");
+   }
+}
+int[] Array = Create(8);
+PrintArray(Array);
